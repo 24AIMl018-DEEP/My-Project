be728716a22fadde82b964c0377e091f8b6b03f1
Kayastha Deep Manish <24aiml018@charusat.edu.in>
2025-01-17 23:51:30 +0530

    This New File Are Use For Js For Index Page And Css File Name Regarding to Index page.

---
+++ js_port_1/index_port.js
@@ -0,0 +1,9 @@
+function fix_element_color_change() {
+    let block1 = document.querySelector(".main_container_index");
+    let block2 = document.querySelector(".main_container_index_content");
+
+    const block1Bottom = block1.getBoundingClientRect().bottom;
+    if(block1Bottom<window)
+    {}  
+    else{}
+}
