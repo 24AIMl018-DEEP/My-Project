e5cd69058c2148662426dbabd9be4643b482363e
Kayastha Deep Manish <24aiml018@charusat.edu.in>
2025-01-18 14:58:24 +0530

    Changing The Postion of System Languages

--- js_port_1/index_port.js
+++
@@ -1,9 +0,0 @@
-function fix_element_color_change() {
-    let block1 = document.querySelector(".main_container_index");
-    let block2 = document.querySelector(".main_container_index_content");
-
-    const block1Bottom = block1.getBoundingClientRect().bottom;
-    if(block1Bottom<window)
-    {}  
-    else{}
-}
